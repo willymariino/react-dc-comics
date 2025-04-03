7370eb1fa282cb6fdfa56f1b5250439a64e0ce01
willymariino <wg.mariino@gmail.com>
2025-04-03 18:55:03 +0200

    added google font

--- DC-comics/src/App.jsx
+++ DC-comics/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Header from './components/header'
+import Main from './components/main'
 
 function App() {
   return (
     <div>
       <Header />
+      <Main />
 
     </div>
 

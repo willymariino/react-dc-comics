106308f58514c17b9e6a8623f7444815a478dc52
willymariino <wg.mariino@gmail.com>
2025-04-03 17:51:30 +0200

    removed import

--- DC-comics/src/App.jsx
+++ DC-comics/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import dc-logo from './public/img/dc-logo.png'
 import Header from './components/header'
 
 function App() {

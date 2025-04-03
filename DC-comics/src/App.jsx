76ec70eaf20eccf6624a5dbbbe91b8515a2f271d
willymariino <wg.mariino@gmail.com>
2025-04-03 17:39:27 +0200

    impoting image

--- DC-comics/src/App.jsx
+++ DC-comics/src/App.jsx
@@ -1,6 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import dc-logo from './public/img/dc-logo.png'
 import Header from './components/header'
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+    </div>
+
+
+
+  )
+}
 
 export default App

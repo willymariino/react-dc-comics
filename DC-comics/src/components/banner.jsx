146424e64a412453f49b8c1b1622953ccd95b81a
willymariino <wg.mariino@gmail.com>
2025-04-03 22:22:53 +0200

    added footer-bottom

--- DC-comics/src/components/banner.jsx
+++ DC-comics/src/components/banner.jsx
@@ -2,19 +2,19 @@ function Banner() {
     return (
         <section className="banner">
             <div className="banner-item">
-                <img src="/img/buy-comics-digital-comics.png" alt="Digital Comics" />
+                <a href="#"><img src="/img/buy-comics-digital-comics.png" alt="Digital Comics" /></a>
                 <p className="descriptor">DIGITAL COMICS</p>
             </div>
             <div className="banner-item">
-                <img src="/img/buy-comics-merchandise.png" alt="DC Merchandise" />
+                <a href="#"> <img src="/img/buy-comics-merchandise.png" alt="DC Merchandise" /> </a>
                 <p className="descriptor">DC MERCHANDISE</p>
             </div>
             <div className="banner-item">
-                <img src="/img/buy-comics-subscriptions.png" alt="Subscription" />
+                <a href="#">  <img src="/img/buy-comics-subscriptions.png" alt="Subscription" /> </a>
                 <p className="descriptor">SUBSCRIPTION</p>
             </div>
             <div className="banner-item">
-                <img src="/img/buy-comics-shop-locator.png" alt="Comic Shop Locator" />
+                <a href="#">  <img src="/img/buy-comics-shop-locator.png" alt="Comic Shop Locator" /> </a>
                 <p className="descriptor">COMIC SHOP LOCATOR</p>
             </div>
             <div className="banner-item">
---
+++ DC-comics/src/components/footer-bottom.jsx
@@ -0,0 +1,29 @@
+function FooterBottom() {
+    return (
+        <div className="bottom-container">
+            <div className="container-button">
+                <button>sign-up now!</button>
+            </div>
+
+            <div className="social">
+                <p>follow us</p>
+                <img src="/img/footer-facebook.png" alt="" />
+                <img src="/img/footer-twitter.png" alt="" />
+                <img src="/img/footer-youtube.png" alt="" />
+                <img src="/img/footer-pinterest.png" alt="" />
+                <img src="/img/footer-periscope.png" alt="" />
+
+
+            </div>
+
+        </div>
+
+
+
+    )
+
+
+
+}
+
+export default FooterBottom

a1080171d04ac4004ee7cd9b740710759fc66450
willymariino <wg.mariino@gmail.com>
2025-04-03 17:27:12 +0200

    created header

--- DC-comics/src/App.jsx
+++ DC-comics/src/App.jsx
@@ -1,10 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-
-
-
-
+import header from './components/header'
 
 export default App

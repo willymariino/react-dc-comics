3c7a0a2cebff128145db029f1a7c3385fc5889c8
willymariino <wg.mariino@gmail.com>
2025-04-04 17:37:13 +0200

    created link array

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -13,16 +13,7 @@ function Navbar() {
     return (
         <nav>
             <ul>
-                <li><a href="#">Characters</a></li>
-                <li><a href="#" className="comics">Comics</a></li>
-                <li><a href="#">Movies</a></li>
-                <li><a href="#">TV</a></li>
-                <li><a href="#">Games</a></li>
-                <li><a href="#">collectibles</a></li>
-                <li><a href="#">videos</a></li>
-                <li><a href="#">fans</a></li>
-                <li><a href="#">news</a></li>
-                <li><a href="#">shop</a></li>
+
             </ul>
 
         </nav>

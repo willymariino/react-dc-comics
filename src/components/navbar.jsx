4337ec00170743d5e71090b83f08586c2af97439
willymariino <wg.mariino@gmail.com>
2025-04-04 17:52:17 +0200

    created array link

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -13,6 +13,16 @@ function Navbar() {
     return (
         <nav>
             <ul>
+                <li><a href="#">{links[0]}</a></li>
+                <li><a href="#">{links[1]}</a></li>
+                <li><a href="#">{links[2]}</a></li>
+                <li><a href="#">{links[3]}</a></li>
+                <li><a href="#">{links[4]}</a></li>
+                <li><a href="#">{links[5]}</a></li>
+                <li><a href="#">{links[6]}</a></li>
+                <li><a href="#">{links[7]}</a></li>
+                <li><a href="#">{links[8]}</a></li>
+                <li><a href="#">{links[9]}</a></li>
 
             </ul>
 

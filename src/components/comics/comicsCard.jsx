a2bf0536827bf5722440f3c57f0a9b1f03c468a1
willymariino <wg.mariino@gmail.com>
2025-04-07 19:18:59 +0200

    css

--- src/components/comics/comicsCard.jsx
+++ src/components/comics/comicsCard.jsx
@@ -2,7 +2,7 @@ function ComicsCard({ title, image }) {
     return (
         <div className="card">
             <img src={image} className="card-image" alt={title} />
-            <h2>{title}</h2>
+            <h2 className="comic-title">{title}</h2>
         </div>
     )
 }

6cec1f98fff2e2eccbe7384386a524e268b70f20
willymariino <wg.mariino@gmail.com>
2025-04-07 19:03:09 +0200

    fixing dimensions

--- src/components/comics/comicsCard.jsx
+++ src/components/comics/comicsCard.jsx
@@ -1,7 +1,7 @@
 function ComicsCard({ title, image }) {
     return (
         <div className="card">
-            <img src={image} alt={title} />
+            <img src={image} className="card-image" alt={title} />
             <h2>{title}</h2>
         </div>
     )
